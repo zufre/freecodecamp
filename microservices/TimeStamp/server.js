add1071733034ba728b0371dbedbbc6b60fc872c
Cédric David <cedric.david1974@gmail.com>
2019-10-19 14:12:26 +1000

    Update server.js

--- microservices/TimeStamp/server.js
+++ microservices/TimeStamp/server.js
@@ -1,102 +1,54 @@
 // init project
 var express = require('express');
-var path = require('path');
 var app = express();
 
-//Use our public files
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-app.get("/", function(req, res){
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-//Returns true if the input is a number
-var isNumeric = function(input) {
-  return !isNaN(parseFloat(input)) && isFinite(input);
-}
 
-//Returns true if the given month is a month
-var validMonth = function(month) {
-  switch(month.toLower())
-      {
-    case "january":
-      break;
-    case "february":
-      break;
-    case "march":
-      break;
-    case "april":
-      break;
-    case "may":
-      break;
-    case "june":
-      break;
-    case "july":
-      break;
-    case "august":
-      break;
-    case "september":
-      break;
-    case "october":
-      break;
-    case "november":
-      break;
-    case "december":
-      break;
+// API endpoint
+app.get("/api/timestamp/:date_string?", function (req, res) {
+  
+  let date = req.params.date_string;
+  let unixDate;
+  let utcDate;
+  
+  if (date === undefined) {
+    unixDate = new Date().getTime();
+    utcDate = new Date().toUTCString();
   }
-}
-
-//Returns true if the format is unix, false if natural, and undefined if neither
-var isUnixTimestamp = function(time)
-{
-  if(isNumeric(time))
-  {
-    return true;
+  else if (new Date(date) && new Date(date) != "Invalid Date") {
+    unixDate = new Date(date).getTime();
+    utcDate = new Date(date).toUTCString();
   }
-  //Check date?
-  return false;
-}
-
-//Given a date or unix time, returns an object with both
-var convertTime = function(time)
-{
-  var isUnix = isUnixTimestamp(time);
-  if(isUnix === undefined)
-    {
-      return {unix: null, natural: null};
-    }
-  if(isUnix)
-    {
-      var date = new Date(parseInt(time));
-      console.log(date);
-      return {unix: time, natural: trimDate(date.toDateString())};
-    }
-  return  {unix: Date.parse(time), natural: time};
-}
-
-//Trims the day off the date. ex: Tuesday January 5, 1999 --> January 5, 1999
-var trimDate = function(dateStr)
-{
-  var i = 0;
-  while(i < dateStr.length && dateStr[i] !== " ")
-    {
-      i++;
-    }
-  i++;
-  return dateStr.substring(i);
-}
-
-//Handle a specific request
-app.get("api/timestamp/:data", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-  //valid url request?
-  if(request.params && Object.keys(request.params).length > 0)
-    {
-      response.write(JSON.stringify(convertTime(request.params.data)));
-    }
-  response.end();
+  
+    
+  else if (Number.isInteger(parseInt(date, 10))) {
+    unixDate = date;
+    utcDate = new Date(parseInt(date, 10)).toUTCString();
+  }
+  else if (unixDate || utcDate  != "Invalid Date") {
+    res.json({error: "Invalid Date"});
+    return;
+  }
+  
+  res.json({unix: unixDate, utc: utcDate});
+  
 });
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
