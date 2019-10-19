e45c88a8aa67fe99bc197705f6baebf9d7ef871d
Cédric David <cedric.david1974@gmail.com>
2019-10-19 14:24:25 +1000

    Create server.js

---
+++ microservices/Request HeaderParserMicroservice/server.js
@@ -0,0 +1,45 @@
+//https://freecodecamp-boilerplate-project-headerparser--8.glitch.me
+
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api/whoami", function (req, res) {
+  var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    var lang = req.headers["accept-language"].split(",")[0];
+    var software = req.headers["user-agent"].match(/\((.+?)\)/)[1];
+  res.json({"ipaddress":ip,
+            "language":lang,
+            "software":software});
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
